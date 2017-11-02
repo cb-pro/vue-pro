6087bae4cb34a042ba78b54215a58b9f0306ff8b
chris <cb@iux.no>
2017-11-02 10:23:58 +0100

    api

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,7 @@ import Landing from '@/views/Landing'
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -1,11 +1,45 @@
 <template lang="html">
   <div class="landing">
-    <h1>Landing.vue</h1>
+    <div v-for="post in posts">
+      <h2>{{ post.title.rendered }}</h2>
+      <p> {{ post.categories }} </p>
+    </div>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
+  data () {
+    return {
+      posts: {
+      },
+      convertCat: [
+        {
+          cats: ['Animals', 'Numbers']
+        }
+      ]
+    }
+  },
+  created: function () {
+    this.getPosts()
+  },
+  methods: {
+    getPosts: function () {
+      let _this = this
+
+      axios.get('http://wp.christofferbogsti.com/wp-json/wp/v2/posts?per_page=5')
+        .then(function (response) {
+        // console.log(response.data)
+          _this.posts = response.data
+        })
+
+        .catch(function (error) {
+          _this.posts = 'An error occured.' + error
+        })
+    }
+  }
 }
 </script>
 
